d09e4814fd35e703b4e491c56e85bc4be361ec98
Nick Engle <enick11@gmail.com>
2018-12-14 15:09:29 -0500

    new file for money stuff

---
+++ money_artifacts.py
@@ -0,0 +1,50 @@
+from number_game import *
+
+'''
+class <Name>(Artifact):
+    def __init__(self, name, rarity, description):
+    
+        super().__init__(name='<name>', 
+                         rarity=<rarity>,
+                         description='<desc>')
+                         
+    def passive(self, owner, T):
+        <fill>
+    
+    def active(self, owner, T):
+        <fill>
+    
+    def make_new():
+        <fill>
+    def to_dict(self):
+        <fill>
+    def from_dict(dic):
+        <fill>
+'''
+
+#A menu that can be used for anything. Options are chosen using their number
+class GeneralPurposeMenu()
+    def __init__(self, options, output_dest, input_dest):
+        
+        
+
+class BankOfUganda(Artifact):
+    def __init__(self, name, rarity, description):
+    
+        super().__init__(name='Bank of Uganda', 
+                         rarity=<rarity>,
+                         description='Gives daily interest.\n' +
+                                     '...maybe not the best place to store your money')
+                         
+    def passive(self, owner, T):
+        <fill>
+    
+    def active(self, owner, T):
+        <fill>
+    
+    def make_new():
+        <fill>
+    def to_dict(self):
+        <fill>
+    def from_dict(dic):
+        <fill>
